ed27776c8c276ef981db0886ef41e28de43d46be
elias alexandre <elliasallex@gmail.com>
2021-11-22 22:11:50 -0300

    chore: document config - v0.0.01

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,9 +6,13 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap"
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Rajdhani:wght@400;600&family=Roboto:ital,wght@1,900&display=swap"
             rel="stylesheet"
           />
         </Head>
